50614fd9055c12577136d63492f07560b5bbed47
Erick <cdzbr46211@gmail.com>
2025-02-02 15:42:39 -0300

    criando nossa requisição http com logica para trazer apenas o pesquisado

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const searchInput = document.getElementById("search-input");
+const resultArtists = document.getElementById("result-artist");
+const resultPlaylists = document.getElementById("result-playlists");
+
+function requestApi(searchValue) {
+  const url = `http://localhost:3000/artists?name_like=${searchValue}`;
+  fetch();
+}
 
 document.addEventListener("input", function () {
-  const searchValue = searchInput.value;
+  const searchValue = searchInput.value.toLowerCase();
+  if (searchValue === "") {
+    resultPlaylists.classList.add("hidden");
+    resultArtists.classList.remove("hidden");
+    return;
+  }
 });
