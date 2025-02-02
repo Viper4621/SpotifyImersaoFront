2e5267cd8df9f1a98aefeff7f6434475e65a0917
Erick <cdzbr46211@gmail.com>
2025-02-02 17:38:23 -0300

    colocamos para exibir pela pesquisa dinamica porem com erro sempre retorna ultimo item do array

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 const searchInput = document.getElementById("search-input");
-const resultArtists = document.getElementById("result-artist");
-const resultPlaylists = document.getElementById("result-playlists");
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById("result-playlists");
 
-function requestApi(searchValue) {
-  const url = `http://localhost:3000/artists?name_like=${searchValue}`;
+function requestApi(searchTerm) {
+  const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
   fetch(url)
-    .then((Response) => Response.json())
+    .then((response) => response.json())
     .then((result) => displayResults(result));
 }
 
-function displayResults() {
-  resultPlaylists.classList.add("hidden");
+function displayResults(result) {
+  resultPlaylist.classList.add("hidden");
   const artistName = document.getElementById("artist-name");
   const artistImage = document.getElementById("artist-img");
+
+  result.forEach((element) => {
+    artistName.innerText = element.name;
+    artistImage.src = element.urlImg;
+  });
+
+  resultArtist.classList.remove("hidden");
 }
 
 document.addEventListener("input", function () {
-  const searchValue = searchInput.value.toLowerCase();
-  if (searchValue === "") {
-    resultPlaylists.classList.add("hidden");
-    resultArtists.classList.remove("hidden");
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultPlaylist.classList.add("hidden");
+    resultArtist.classList.add("hidden");
     return;
   }
-  requestApi(searchValue);
+
+  requestApi(searchTerm);
 });
