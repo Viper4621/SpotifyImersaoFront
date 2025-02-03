358724306f90bb790b8c3ddcfc53cb2520c2c75b
Erick <cdzbr46211@gmail.com>
2025-02-03 15:47:41 -0300

    criando componente Footer e estilizando

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header/Header";
 import Sidebar from "../src/Sidebar/Sidebar";
 import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Sidebar />
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
