57d87b08431c1fe53ccd59f641f39f581ae5ce07
Erick <cdzbr46211@gmail.com>
2025-02-03 11:13:42 -0300

    renomeando class para className

--- spotify-react/src/Header/Header.js
+++ spotify-react/src/Header/Header.js
@@ -1,4 +1,37 @@
 import React from "react";
 import "./Header.css";
 
-const Header
+const Header = () => {
+  return (
+    <nav className="header__navigation">
+      <div className="navigation">
+        <button className="arrow-left">
+          <img
+            src="./src/assets/icons/small-left.png"
+            alt="seta para esquerda"
+          />
+        </button>
+        <button className="arrow-right">
+          <img
+            src="./src/assets/icons/small-right.png"
+            alt="seta para direita"
+          />
+        </button>
+      </div>
+      <div className="header__search">
+        <img src="./src/assets/icons/search.png" alt="" />
+        <i
+          nput
+          id="search-input"
+          type="text"
+          maxlength="800"
+          placeholder="O que você quer ouvir?"
+        />
+      </div>
+      <div className="header__login">
+        <button className="subscribe">Inscreva-se</button>
+        <button className="login">Entrar</button>
+      </div>
+    </nav>
+  );
+};
