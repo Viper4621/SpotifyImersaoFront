bd8c71b88341272082b7464cc89f978b7c138a7e
Erick <cdzbr46211@gmail.com>
2025-02-03 15:22:44 -0300

    criando componente main

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Header/Header";
 import Sidebar from "../src/Sidebar/Sidebar";
+import Main from "./Main/Main";
 
 function App() {
   return (
     <div>
       <Sidebar />
       <Header />
+      <Main />
     </div>
   );
 }
